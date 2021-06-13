add46bc302c6b79ccb617fa3553791bd852cf0ac
William Hexberg <whexberg@gmail.com>
2021-06-12 23:43:51 -0700

    Add css, sass, postcss, hotreloading

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,24 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const loader = require("sass-loader");
+
 let mode = "development";
+let target = "web";
 
 if (process.env.NODE_ENV === "production") {
     mode = "production";
+    target = "browserslist";
 }
 
 module.exports = {
     mode,
+    target,
 
     module: {
         rules: [
+            {
+                test: /\.(s[ca]|c)ss$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
+            },
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
@@ -17,8 +27,11 @@ module.exports = {
         ],
     },
 
+    plugins: [new MiniCssExtractPlugin()],
+
     devtool: "source-map",
     devServer: {
         contentBase: "./dist",
+        hot: true,
     },
 };
