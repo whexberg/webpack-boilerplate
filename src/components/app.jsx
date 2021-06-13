825078165b0abca597fc800b2a031b2e0b368807
William Hexberg <whexberg@gmail.com>
2021-06-13 16:32:35 -0700

    Add image support to webpack

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,8 @@
 import "../styles/index.scss";
 
 import { Recipes } from "./recipes";
+import sword from "../images/swc-sword.png";
+import swordSvg from "../images/sword.svg";
 
 export const App = () => {
     return (
@@ -10,8 +12,10 @@ export const App = () => {
                 <section>
                     <h1>Oh hai, React</h1>
                 </section>
+                <img src={sword} alt="sword" width="250" />
+                <img src={swordSvg} alt="sword" width="250" />
+                <Recipes />
             </main>
-            <Recipes />
         </>
     );
 };
--- webpack.config.js
+++ webpack.config.js
@@ -12,17 +12,23 @@ module.exports = {
     mode,
     target,
 
+    output: {
+        assetModuleFilename: "images/[hash][ext][query]",
+    },
+
     module: {
         rules: [
+            { test: /\.(png|jpe?g|gif|svg)$/i, type: "asset", parser: { dataUrlCondition: { maxSize: 30 * 1024 } } },
             {
                 test: /\.(s[ca]|c)ss$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
+                use: [
+                    { loader: MiniCssExtractPlugin.loader, options: { publicPath: "" } },
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader",
+                ],
             },
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                use: { loader: "babel-loader" },
-            },
+            { test: /\.jsx?$/, exclude: /node_modules/, use: { loader: "babel-loader" } },
         ],
     },
 
